c5a1d306509289104d87090b6f25a70383a100bd
Sebastian Małek <sebastian.malek@fajne.to>
2024-06-15 13:17:28 +0200

    forgot about routing orders and ignor sqlite file

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException
 
 from models.db import Base, engine
-from routers import articles, users
+from routers import articles, users, orders
 
 app = FastAPI()
 
-
-
-
-# Create tables
-Base.metadata.create_all( bind=engine )
+Base.metadata.create_all(bind=engine)
 
 app.include_router(users.router)
 app.include_router(articles.router)
+app.include_router(orders.router)
 
 
 @app.get("/")
